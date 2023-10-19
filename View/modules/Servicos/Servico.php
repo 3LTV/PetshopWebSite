a2afc17803c852e30435d2cb39a1fd29b561ce32
thiag0-dot <thiagodeginofilho@gmail.com>
2023-10-19 15:50:32 -0300

    Feat:Começando a fazer arquivos do serviços

--- View/modules/Servicos/Servico.php
+++ View/modules/Servicos/Servico.php
@@ -25,6 +25,36 @@
             </div>
         </nav>
     </header>
+    <div class="form">
+        <form action="" class="sub-form">
+            <h1>Serviços</h1><br>
+                <div class="form-top">
+                    <div class="control-group">
+                        <input type="checkbox" name="banho" id="banho">
+                        <label for="banho">Banho</label>
+                    </div>
+                    <div class="control-group">
+                        <input type="checkbox" name="hidratacao" id="hidratacao">
+                        <label for="hidratacao">Hidratação</label>
+                    </div>
+                    <div class="control-group">
+                        <input type="checkbox" name="unha" id="unha">
+                        <label for="unha">Corte de Unhas</label>
+                    </div>
+                    <div class="control-group">
+                        <input type="checkbox" name="dentes" id="dentes">
+                        <label for="dentes">Escovar os Dentes</label>
+                    </div>
+                    <div class="control-group">
+                        <input type="checkbox" name="desembolo" id="desembolo">
+                        <label for="desembolo">Desembolo</label>
+                    </div>
+                    <div class="control ">
+                        <input type="text" disabled>
+                    </div>
+                </div>  
+        </form>
+    </div>
     <script src="../Servicos/script.js"></script>
 </body>
 </html>
